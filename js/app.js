86ef2fe08844e07c15299d75b24e85707f6c71ec
willsmaniotto <smaniottowillian@gmail.com>
2018-11-29 17:59:17 -0200

    type: feat
    Foi adicionado eventos de clique as cartas na função "click" e a funcionalidade de exibir o conteúdo da carta através da função "showSymbol".

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 /*
  * Create a list that holds all of your cards
  */
+ const box=document.querySelector('.deck');
+ var cards=document.querySelectorAll(".card");
+ var openCards=0;
 
 
 /*
@@ -9,6 +12,26 @@
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
+function start(){
+	//Remove todos as cartas da tela
+	for (var i=0; i<cards.length; i++){
+		cards[i].remove();
+	}
+	//Embaralha as carts no array
+	cards=shuffle(Array.from(cards));
+	//Percorre o array
+	for (i=0; i<cards.length; i++){
+		//Cria o elemento da carta
+		var newCard = document.createElement('li');
+		//Indica que todas as cartas devem aparecer escondidas
+		cards[i].className="card";
+		//Passa todos atributos para o elemento carta criado
+		newCard=cards[i];
+		//Adiciona o elemento carta ao quadro do jogo
+		box.appendChild(newCard);
+	}
+}
+
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -21,10 +44,26 @@ function shuffle(array) {
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
     }
-
+    console.log(array);
     return array;
 }
 
+//Apresenta a carta desvirada
+function showSymbol(card){
+	card.className+=" open show";
+}
+
+//Adiciona eventos de cliques as cartas do jogo
+function click(){
+	//Percorre todas as cartas
+	for (var i=0; i<cards.length; i++){
+		//Adiciona evento de click a todas as cartas
+		cards[i].addEventListener('click', function (event){
+			//Chama função para exibir a carta clicada
+			showSymbol(event.target);
+		});
+	}
+}
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
@@ -35,3 +74,8 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+//Inicializa o jogo embaralho e virando todas as cartas
+ start();
+ //Adiciona eventos de clique as cartas
+ click();
